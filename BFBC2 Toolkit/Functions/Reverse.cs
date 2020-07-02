1cbe0dcd3385111955ffcacd0a7432b155375716
Heico <HeicoDev@users.noreply.github.com>
2020-07-03 00:12:30 +0200

    Added basic code template for upcoming File Porter

---
+++ BFBC2 Toolkit/Functions/Reverse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BFBC2_Toolkit.Functions
+{
+    public class Reverse
+    {
+        //Not sure if taking and returning the whole data is efficient in general. Probably not, but it is good enough for our case. Might improve it later.
+        public static byte[] TwoByteBlock(byte[] data, int offset, int length)
+        {
+            byte temp0 = data[offset],
+                 temp1 = data[offset + 1];
+
+            data[offset] = temp1;
+            data[offset + 1] = temp0;
+
+            Console.WriteLine("Writing Bytes: " + offset + " of " + length + " Data: " + temp1 + " " + temp0);
+
+            return data;
+        }
+
+        public static byte[] FourByteBlock(byte[] data, int offset, int length)
+        {
+            byte temp0 = data[offset],
+                 temp1 = data[offset + 1],
+                 temp2 = data[offset + 2],
+                 temp3 = data[offset + 3];
+
+            data[offset] = temp3;
+            data[offset + 1] = temp2;
+            data[offset + 2] = temp1;
+            data[offset + 3] = temp0;
+
+            Console.WriteLine("Writing Bytes: " + offset + " of " + length + " Data: " + temp3 + " " + temp2 + " " + temp1 + " " + temp0);
+
+            return data;
+        }
+    }
+}
--- BFBC2 Toolkit/Tools/FilePorter.cs
+++ BFBC2 Toolkit/Tools/FilePorter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BFBC2_Toolkit.Functions;
 
 /// <summary>
 /// BFBC2 File Porter
@@ -15,6 +17,192 @@ namespace BFBC2_Toolkit.Tools
 {
     public class FilePorter
     {
+        public static void ConvertFile(string filePath)
+        {
+            try
+            {
+                byte[] fileData = { };
 
+                using (var br = new BinaryReader(File.OpenRead(filePath)))
+                {
+                    fileData = br.ReadBytes(Convert.ToInt32(br.BaseStream.Length));
+                }
+
+                int offset = 0;
+                int length = fileData.Length;
+
+                string fileExtension = String.Empty;
+
+                if (filePath.EndsWith(".terrainheightfield"))
+                {
+                    //Heightmap (WIP)
+                    fileExtension = ".terrainheightfield";
+
+                    //Reverse header
+
+                    //Reverse first data chunk
+                    while (true)
+                    {
+                        if (offset >= length)
+                            break;
+
+                        fileData = Reverse.TwoByteBlock(fileData, offset, length);
+
+                        offset += 2;
+                    }
+
+                    //Reverse second data chunk
+                }
+                else if (filePath.EndsWith(".watermesh"))
+                {
+                    //WaterMesh
+                    fileExtension = ".watermesh";
+
+                    //Whole file including header can be reversed in 4 byte blocks
+                    while (true)
+                    {
+                        if (offset >= length)
+                            break;
+
+                        fileData = Reverse.FourByteBlock(fileData, offset, length);
+
+                        offset += 4;
+                    }
+                }
+                else if (filePath.EndsWith(".visualwater"))
+                {
+                    //VisualWater
+                    fileExtension = ".visualwater";
+
+                    byte[] pointer = { fileData[11], fileData[10], fileData[9], fileData[8] };
+
+                    int pointerOffset = BitConverter.ToInt32(pointer, 0);
+
+                    //Reverse header
+                    for (int i = 0; i < 3; i++)
+                    {
+                        fileData = Reverse.FourByteBlock(fileData, offset, length);
+
+                        offset += 4;
+                    }
+
+                    //Reverse first data chunk
+                    while (true)
+                    {
+                        if (offset == pointerOffset)
+                        {
+                            fileData = Reverse.TwoByteBlock(fileData, offset, length);
+                            fileData = Reverse.TwoByteBlock(fileData, offset + 2, length);
+
+                            offset += 4;
+
+                            break;
+                        }
+
+                        fileData = Reverse.TwoByteBlock(fileData, offset, length);
+
+                        offset += 2;
+                    }
+
+                    //Reverse second data chunk
+                    int materialsCount;
+
+                    while (true)
+                    {
+                        if (fileData[offset] == 84 && fileData[offset + 1] == 101 && fileData[offset + 2] == 114 && fileData[offset + 3] == 114 ||
+                            fileData[offset] == 83 && fileData[offset + 1] == 104 && fileData[offset + 2] == 97 && fileData[offset + 3] == 100)
+                        {
+                            materialsCount = BitConverter.ToInt32(fileData, offset - 4);
+                            offset += 4;
+                            break;
+                        }
+
+                        fileData = Reverse.FourByteBlock(fileData, offset, length);
+
+                        offset += 4;
+                    }
+
+                    //Skip strings/materials
+                    while (true)
+                    {
+                        if (fileData[offset] == 0 && fileData[offset + 1] == 0)
+                            materialsCount--;
+
+                        if (materialsCount == 0)
+                        {
+                            offset++;
+                            break;
+                        }
+
+                        offset++;
+                    }
+
+                    //Reverse actual footer
+                    while (true)
+                    {
+                        if (offset >= length)
+                            break;
+
+                        fileData = Reverse.FourByteBlock(fileData, offset, length);
+
+                        offset += 4;
+                    }
+                }
+                else if (filePath.EndsWith(".terrainmaterialmap"))
+                {
+                    //TerrainMaterialMap (WIP)
+                    fileExtension = ".terrainmaterialmap";
+
+                }
+                else if (filePath.EndsWith(".visualterrain"))
+                {
+                    //VisualTerrain (WIP)
+                    fileExtension = ".visualterrain";
+
+                }
+                else if (filePath.EndsWith(".ps3texture") || filePath.EndsWith(".xenontexture"))
+                {
+                    //Texture
+                    fileExtension = ".itexture";
+
+                    //Reverse header
+                    for (int i = 0; i < 23; i++)
+                    {
+                        fileData = Reverse.FourByteBlock(fileData, offset, length);
+
+                        offset += 4;
+                    }
+
+                    //Reverse data
+                    while (true)
+                    {
+                        if (offset >= length)
+                            break;
+
+                        fileData = Reverse.TwoByteBlock(fileData, offset, length);
+
+                        offset += 2;
+                    }
+                }
+
+                string outputFile = "output" + fileExtension;
+
+                if (File.Exists(outputFile))
+                    File.Delete(outputFile);
+
+                using (var bw = new BinaryWriter(File.OpenWrite(outputFile)))
+                {
+                    bw.BaseStream.Position = 0x0;
+                    bw.Write(fileData);
+                }
+
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+        }
     }
 }
